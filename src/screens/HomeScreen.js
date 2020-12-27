0b5cf17e35d872ee7fc350c0bc9ea140069bde33
petar-zinaic <petar.zinaic@marbleit.rs>
2020-12-27 16:07:23 +0100

    Implement home page

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,36 +1,78 @@
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import firebase from "firebase";
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import {windowHeight, windowWidth} from '../utils/Dimensions';
+import firebase from 'firebase';
 
 const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.topContainer}>
+        <Image
+          source={require('../assets/icons/flame-logo.png')}
+          style={styles.logo}
+        />
+        <Text style={styles.text}>Welcome to {'\n'} Employee Manager</Text>
+      </View>
 
-    return(
-        <View style={styles.container}>
-            <Text>Home Screen</Text>
-
-            <TouchableOpacity
-             style={styles.logoutButton}
-             onPress={() => firebase.auth().signOut()}
-             >
-                <Text>Logout</Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
+      <View style={styles.bottomContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Employee List</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => firebase.auth().signOut()}>
+          <Text style={styles.buttonText}>Logout</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    logoutButton: {
-        paddingTop:15,
-        paddingBottom: 15,
-        paddingLeft: 40,
-        paddingRight: 40,
-        backgroundColor: 'red',
-        marginTop: 20
-    }
-})
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: windowHeight,
+  },
+  topContainer: {
+    height: windowHeight / 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bottomContainer: {
+    paddingTop: 20,
+    height: windowHeight / 2,
+  },
+  button: {
+    marginTop: 10,
+    width: windowWidth / 1.5,
+    height: windowHeight / 15,
+    backgroundColor: '#2e64e5',
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 3,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#ffffff',
+    fontFamily: 'Lato-Regular',
+  },
+  text: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    fontFamily: 'Lato-Regular',
+    color: '#2e64e5',
+    textAlign: 'center',
+    padding: 20,
+    paddingBottom: 2,
+    marginTop: 10,
+  },
+  logo: {
+    height: 250,
+    width: 250,
+  },
+});
 
 export default HomeScreen;
