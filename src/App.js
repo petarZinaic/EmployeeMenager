7a2aca080be99e25ef6921738484696ef06006c7
petar-zinaic <petar.zinaic@marbleit.rs>
2021-01-10 18:34:00 +0100

    Finish login reducers

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import React, { Component }  from 'react';
-import { View, Text } from 'react-native';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducers from './reducers';
 import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
   componentDidMount() {
     const config = {
-      apiKey: "AIzaSyBh97DGahbhm9wX8Gb1rbIUlj75B9Xny38",
-      authDomain: "manager-75295.firebaseapp.com",
-      projectId: "manager-75295",
-      storageBucket: "manager-75295.appspot.com",
-      messagingSenderId: "678571542819",
-      appId: "1:678571542819:web:96b623c699851f7d9a2496",
-      measurementId: "G-RZC2BSP041"
+      apiKey: 'AIzaSyBh97DGahbhm9wX8Gb1rbIUlj75B9Xny38',
+      authDomain: 'manager-75295.firebaseapp.com',
+      projectId: 'manager-75295',
+      storageBucket: 'manager-75295.appspot.com',
+      messagingSenderId: '678571542819',
+      appId: '1:678571542819:web:96b623c699851f7d9a2496',
+      measurementId: 'G-RZC2BSP041',
     };
-    if(!firebase.app.length) {
+    if (!firebase.apps.length) {
       firebase.initializeApp(config);
-
+    } else {
+      firebase.app();
     }
   }
 
   render() {
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
         <LoginForm />
       </Provider>
-    )
+    );
   }
 }
 
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,50 @@
-import { EMAIL_CHANGED } from './types';
-import { PASSWORD_CHANGED } from './types';
+import firebase from 'firebase';
+import {EMAIL_CHANGED} from './types';
+import {PASSWORD_CHANGED} from './types';
+import {LOGIN_USER_SUCCESS} from './types';
+import { LOGIN_USER_FAIL } from './types';
+import { LOGIN_USER } from './types';
 
-
-
-export const emailChanged = text => {
-    return {
-        type: EMAIL_CHANGED,
-        payload: text
-    };
+export const emailChanged = (text) => {
+  return {
+    type: EMAIL_CHANGED,
+    payload: text,
+  };
 };
 
-export const passwordChanged = text => {
-    return {
-        type: PASSWORD_CHANGED,
-        payload: text
-    };
+export const passwordChanged = (text) => {
+  return {
+    type: PASSWORD_CHANGED,
+    payload: text,
+  };
 };
 
+export const loginUser = ({email, password}) => {
+  return (dispatch) => {
+    // if(!firebase.app.length) {
+      
+    // }
+
+    dispatch({ type: LOGIN_USER })
+  
+      firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => loginUserSucess(dispatch, user))   
+      .catch((error) => {
+          firebase.auth().createUserWithEmailAndPassword(email, password)
+       .then(user => loginUserSucess(dispatch, user))
+       .catch(() => loginUserFail(dispatch))  
+      })  
+  
+  };
+};
+
+const loginUserFail = (dispatch) => {
+    dispatch({ type: LOGIN_USER_FAIL });
+}
+
+const loginUserSucess = (disptach, user) => {
+  disptach({
+    type: LOGIN_USER_SUCCESS,
+    payload: user,
+  });
+};
